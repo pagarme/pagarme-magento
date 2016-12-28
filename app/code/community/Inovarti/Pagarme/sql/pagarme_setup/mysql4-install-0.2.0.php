ee8b486d21a2458ebfac8b10d16d9a8b6be65209
Davi Marcondes Moreira <davi.marcondes.moreira@gmail.com>
2016-12-28 15:05:55 -0200

    Fix for error when editing website split rules.

--- app/code/community/Inovarti/Pagarme/sql/pagarme_setup/mysql4-install-0.2.0.php
+++ app/code/community/Inovarti/Pagarme/sql/pagarme_setup/mysql4-install-0.2.0.php
@@ -360,6 +360,7 @@ if(!$installer->getConnection()->isTableExists($installer->getTable('pagarme_spl
     $table = $installer->getConnection()
         ->newTable($installer->getTable('pagarme_split_rules_group'))
         ->addColumn('entity_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+            'identity' => true,
             'nullable' => false,
             'unsigned' => true,
             'primary' => true
