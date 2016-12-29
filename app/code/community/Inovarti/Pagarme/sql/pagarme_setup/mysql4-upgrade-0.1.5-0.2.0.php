492aec93af2288566a58002b001fa42177053022
Davi Marcondes Moreira <davi.marcondes.moreira@gmail.com>
2016-12-29 15:37:45 -0200

    Fixes and improvements

--- app/code/community/Inovarti/Pagarme/sql/pagarme_setup/mysql4-upgrade-0.1.5-0.2.0.php
+++ app/code/community/Inovarti/Pagarme/sql/pagarme_setup/mysql4-upgrade-0.1.5-0.2.0.php
@@ -6,26 +6,46 @@ $installer->startSetup();
 $table = $installer->getTable('pagarme_split_rules');
 
 $installer->getConnection()
-    ->addColumn($table, 'group_id', array(
-        'type'     => Varien_Db_Ddl_Table::TYPE_INTEGER,
-        'unsigned' => true,
-        'nullable' => true,
-        'comment'  => 'Group ID'
-    ));
+    ->addColumn(
+        $table,
+        'group_id',
+        array(
+            'type' => Varien_Db_Ddl_Table::TYPE_INTEGER,
+            'unsigned' => true,
+            'nullable' => true,
+            'comment' => 'Group ID'
+        )
+    );
 
 if(!$installer->getConnection()->isTableExists($installer->getTable('pagarme_split_rules_group'))) {
     $table = $installer->getConnection()
         ->newTable($installer->getTable('pagarme_split_rules_group'))
-        ->addColumn('entity_id', Varien_Db_Ddl_Table::TYPE_VARCHAR, 255, array(
-            'nullable' => false,
-            'primary' => true
-                ), 'Id')
-        ->addColumn('group_name', Varien_Db_Ddl_Table::TYPE_VARCHAR, 255, array(
-            'nullable' => false
-                ), 'Group Name')
-        ->addColumn('website_id', Varien_Db_Ddl_Table::TYPE_VARCHAR, 255, ARRAY(
-            'nullable' => false
-                ), 'Website ID');
+        ->addColumn(
+            'entity_id',
+            Varien_Db_Ddl_Table::TYPE_VARCHAR,
+            255,
+            array(
+                'identity' => true,
+                'nullable' => false,
+                'unsigned' => true,
+                'primary' => true
+            ),
+            'Id'
+        )
+        ->addColumn(
+            'group_name',
+            Varien_Db_Ddl_Table::TYPE_VARCHAR,
+            255,
+            array('nullable' => false),
+            'Group Name'
+        )
+        ->addColumn(
+            'website_id',
+            Varien_Db_Ddl_Table::TYPE_VARCHAR,
+            255,
+            array('nullable' => false),
+            'Website ID'
+        );
 
     $this->getConnection()->createTable($table);
 }
