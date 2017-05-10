0e32789dce23a314a8a3ed44037067649d443bd7
Davi Marcondes Moreira <davi.marcondes.moreira@gmail.com>
2017-05-10 16:56:13 -0300

    OSC scenarios will run apart from all other tests.
    This way, we can a) run all tests, b) install OSC and then c) run OSC tests.

--- script/test-step2-e2e.sh
+++ script/test-step2-e2e.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 docker-compose exec -T magento vendor/bin/behat
+
+docker-compose exec magento bash -c 'export INSTALL_INOVARTI=true && source /opt/docker/scripts/modules && installOSC'
+
+docker-compose exec -T magento vendor/bin/behat --tags=OSC
