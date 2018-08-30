76eaf5f2e0a360a154d97059f8cdc368c3249768
Leonam Pereira Dias <leonam.dias@pagar.me>
2018-08-30 11:12:37 -0300

    postback: cancel an order
    Cancel an order using its method. This is necessary to follow the right
    workflow, including restore products to stock

--- app/code/community/PagarMe/Core/Model/PostbackHandler/Refused.php
+++ app/code/community/PagarMe/Core/Model/PostbackHandler/Refused.php
@@ -15,18 +15,33 @@ class PagarMe_Core_Model_PostbackHandler_Refused extends PagarMe_Core_Model_Post
     }
 
     /**
+     * Cancel an order with custom message
+     *
+     * @throws \Mage_Core_Exception
+     */
+    private function cancel()
+    {
+        if ($this->order->canCancel()) {
+            $this->order->getPayment()->cancel();
+            $this->order->registerCancellation(
+                Mage::helper('pagarme_core')->__('Refused by gateway.')
+            );
+
+            Mage::dispatchEvent(
+                'order_cancel_after',
+                ['order' => $this->order]
+            );
+        }
+    }
+
+    /**
      * @return \Mage_Sales_Model_Order
      */
     public function process()
     {
         $transaction = Mage::getModel('core/resource_transaction');
 
-        $this->order->setState(
-            Mage_Sales_Model_Order::STATE_CANCELED,
-            true,
-            Mage::helper('pagarme_core')->
-            __('Refused by gateway.')
-        );
+        $this->cancel();
 
         $transaction->addObject($this->order)->save();
 
