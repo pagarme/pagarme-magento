3f0a9c2ad4e2cb80c448da1e8f40266c6441697a
Murilo Henrique <murilohns@gmail.com>
2020-05-08 16:07:17 -0300

    postback: pending_review and analyzing to paid
    fix paid postback when the old status is pending_review or analyzing

--- app/code/community/PagarMe/Core/Model/PostbackHandler/Analyzing.php
+++ app/code/community/PagarMe/Core/Model/PostbackHandler/Analyzing.php
@@ -5,7 +5,7 @@ use \PagarMe\Sdk\Transaction\AbstractTransaction;
 class PagarMe_Core_Model_PostbackHandler_Analyzing extends PagarMe_Core_Model_PostbackHandler_Base
 {
     const MAGENTO_DESIRED_STATUS = Mage_Sales_Model_Order::STATE_PAYMENT_REVIEW;
-   
+
     /**
      * Returns the desired state on magento
      *
@@ -22,7 +22,10 @@ class PagarMe_Core_Model_PostbackHandler_Analyzing extends PagarMe_Core_Model_Po
     public function process()
     {
         $this->order->setState(
-            self::MAGENTO_DESIRED_STATUS
+            self::MAGENTO_DESIRED_STATUS,
+            true,
+            Mage::helper('pagarme_core')
+                ->__('Transaction waiting for antifraud analysis')
         );
 
         Mage::getModel('core/resource_transaction')
